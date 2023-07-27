25269df15b7ac3cd866c884ad6dfbae64433a72b
Stan1slawww <stas.gulnik.04@bk.ru>
2023-07-27 23:44:23 +0300

    init:add text

---
+++ library/index.js
@@ -0,0 +1,1 @@
+console.log("1. Вёрстка валидная - 0 \n 2. Вёрстка семантическая - 16 \n 3.Вёрстка по макету - 42: \n header - 8 \n welcome - 4 \n about - 6 \n favorites - 4 \n coffeshop - 6 \n contacts - 6 \n librarycard - 4 \n footer - 4 \n 4. Общие треюования - 10" )
