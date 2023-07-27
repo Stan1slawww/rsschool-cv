f2dd8058fcd908dd8ba5b2b2adc7a20473b1b8ad
Stan1slawww <stas.gulnik.04@bk.ru>
2023-07-28 00:20:15 +0300

    fix: fix text

--- library/index.js
+++ library/index.js
@@ -1,13 +1,1 @@
-console.log(' \n
-1. Вёрстка валидная - 0 \n
-2. Вёрстка семантическая - 16 \n
-3.Вёрстка по макету - 42: \n
- header - 8 \n
-  welcome - 4 \n
-   about - 6 \n
-    favorites - 4 \n
-     coffeshop - 6 \n
-      contacts - 6 \n
-       librarycard - 4 \n
-        footer - 4 \n
- 4. Общие треюования - 10')
+console.log('\n1. Вёрстка валидная - 0 \n2. Вёрстка семантическая - 16 \n3.Вёрстка по макету - 42: \nheader - 8 \nwelcome - 4 \nabout - 6 \nfavorites - 4 \ncoffeshop - 6 \ncontacts - 6 \nlibrarycard - 4 \nfooter - 4 \n4. Общие треюования - 10')
